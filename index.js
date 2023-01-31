1b1319c6219b36f1b72352a3ffca22d919b37e32
Saki <sbasken@gmail.com>
2023-01-30 21:50:48 -0800

    complete functions tests

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun (activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+const mondayWork = function(x="go to the office") {
+    return "This Monday, I will " + x + "."
+}
+
+function wrapAdjective(string = "*") {
+    return function (words = "special") {
+        return "You are " + string + words + string + "!"
+        }
+    }
+
+
+wrapAdjective("%")("a dedicated programmer")
